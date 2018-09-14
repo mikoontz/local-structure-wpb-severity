a39faf1f04729f55008a1a1f1fb27f5ca3465199
Michael Koontz <mikoontz@gmail.com>
2018-09-14 15:28:22 -0600

    refactor code to use the much *much* faster velox package for working with raster data in R. (processing all 22 of these sites took 50 minutes, whereas trying to use raster, tabularaster, and dplyr didn't even finish 1 in 10 hours)

--- data/data_carpentry/classify_crown-segments.R
+++ data/data_carpentry/classify_crown-segments.R
@@ -3,10 +3,91 @@ library(raster)
 library(tabularaster)
 library(tidyverse)
 library(viridis)
+library(purrr)
+library(velox)
+library(viridis)
 
 
 # character vector of all study sites
 all_sites <- list.files("data/data_output/site_data")
+
+overwrite <- FALSE
+
+if (!file.exists("data/data_output/classified/hand-classified/hand-classified-and-extracted.rda") | overwrite) {
+  
+  hand_classified_site_names <- substr(x = list.files("data/data_output/classified/hand-classified/crown-shapefiles"), start = 1, stop = 9)
+  
+  hand_classified_ttops <- 
+    hand_classified_site_names %>% 
+    map(.f = function(x) {
+      sf::st_read(paste0("data/data_output/site_data/", x, "/", x, "_ttops/", x, "_ttops.shp"))
+    })
+  
+  hand_classified_crowns <-
+    hand_classified_site_names %>% 
+    map2(.y = hand_classified_ttops, .f = function(x, y) {
+      crowns <- 
+        sf::st_read(paste0("data/data_output/classified/hand-classified/crown-shapefiles/", x, "_classified-crowns/", x, "_classified-crowns.shp")) %>% 
+        dplyr::mutate(site = x) %>% 
+        dplyr::mutate(crs = st_crs(.)$proj4string) %>%
+        dplyr::mutate(treeID = y$treeID) %>% 
+        dplyr::mutate(ttop_x = st_coordinates(y)[, "X"]) %>% 
+        dplyr::mutate(ttop_y = st_coordinates(y)[, "Y"]) %>% 
+        dplyr::filter(!is.na(live)) %>% 
+        dplyr::mutate(object_ = 1:nrow(.))
+    })
+
+  hand_classified_orthos <-
+    hand_classified_site_names %>% 
+    map(.f = function(x) {
+      raster::brick(paste0("data/data_output/site_data/", x, "/3_dsm_ortho/2_mosaic/", x, "_x3_transparent_mosaic_group1.tif"))
+    })
+  
+  hand_classified_crowns <- 
+    hand_classified_crowns %>% 
+    map2(.y = hand_classified_orthos, .f = function(crowns, ortho) {
+      live_or_dead <- 
+        cellnumbers(ortho[[1]], crowns) %>% 
+        dplyr::mutate(r = raster::extract(ortho[[1]], cell_)) %>% 
+        dplyr::mutate(g = raster::extract(ortho[[2]], cell_)) %>% 
+        dplyr::mutate(b = raster::extract(ortho[[3]], cell_)) %>% 
+        dplyr::mutate(rgi = r/g) %>% 
+        dplyr::mutate(rgvi = (r - g) / (r + g)) %>% 
+        dplyr::mutate(gbi = g/b) %>% 
+        dplyr::mutate(gbvi = (g - b) / (g + b)) %>% 
+        dplyr::group_by(object_) %>% 
+        dplyr::summarize(r_mean = mean(r, na.rm = TRUE),
+                         g_mean = mean(g, na.rm = TRUE),
+                         b_mean = mean(b, na.rm = TRUE),
+                         rgi_mean = mean(rgi, na.rm = TRUE),
+                         rgvi_mean = mean(rgvi, na.rm = TRUE),
+                         gbi_mean = mean(gbi, na.rm = TRUE),
+                         gbvi_mean = mean(gbvi, na.rm = TRUE),
+                         r_hi = quantile(r, probs = 0.75, na.rm = TRUE),
+                         g_hi = quantile(g, probs = 0.75, na.rm = TRUE),
+                         b_hi = quantile(b, probs = 0.75, na.rm = TRUE),
+                         rgi_hi = quantile(rgi, probs = 0.75, na.rm = TRUE),
+                         rgvi_hi = quantile(rgvi, probs = 0.75, na.rm = TRUE),
+                         gbi_hi = quantile(gbi, probs = 0.75, na.rm = TRUE),
+                         gbvi_hi = quantile(gbvi, probs = 0.75, na.rm = TRUE))
+      
+      crowns <- 
+        crowns %>% 
+        left_join(live_or_dead, by = "object_")
+    })
+  
+  save(hand_classified_crowns, file = "data/data_output/classified/hand-classified/hand-classified-and-extracted.rda")
+}
+
+# Object name is "hand_classified_crowns"
+load("data/data_output/classified/hand-classified/hand-classified-and-extracted.rda")
+hand_classified_crowns
+
+hcc_data <- lapply(hand_classified_crowns, FUN = as.data.frame) %>% 
+  do.call(rbind, .)
+
+m1 <- glm(live ~ r_mean + g_mean + b_mean + rgi_mean + gbi_mean, family = "binomial", data = hcc_data)
+summary(m1)
 
 sites_checklist <- 
   expand.grid(
@@ -22,7 +103,7 @@ sites_checklist <-
   dplyr::select(-forest, -elev, -rep) %>% 
   dplyr::mutate(ttops_check = file.exists(paste0("data/data_output/site_data/", site, "/", site, "_ttops/", site, "_ttops.shp"))) %>% 
   dplyr::mutate(crowns_check = file.exists(paste0("data/data_output/site_data/", site, "/", site, "_crowns/", site, "_crowns.shp"))) %>% 
-  dplyr::mutate(classified_check = file.exists(paste0("data/data_output/site_data/", site, "/", site, "_classified/", site, "_classified.shp")))
+  dplyr::mutate(classified_check = file.exists(paste0("data/data_output/classified/model-classified/crown-shapefiles/", site, "_classified-crowns/", site, "_classified-crowns.shp")))
 
 sites_to_process <-
   sites_checklist %>% 
@@ -30,112 +111,71 @@ sites_to_process <-
   dplyr::select(site) %>% 
   dplyr::pull()
 
-hand_classified_sites <-
-  list.files("data/data_output/site_data")
-  sf::st_read("data/data_output/site_data/eldo_3k_1/eldo_3k_1_live-or-dead/eldo_3k_1_live-or-dead.shp")
+overwrite <- FALSE
 
-i = 1
-for (i in seq_along(sites_to_process)) {
-  # get the character string representing the ith site
-  current_site <- sites_to_process[i]
-  
-# convenience string to set file paths for input/output
-current_dir <- paste0("data/data_output/site_data/", current_site, "/")
+  if (overwrite) {
+    sites_to_process %>% 
+      map(.f = function(site) {
+        if (dir.exists(paste0("data/data_output/classified/model-classified/crown-shapefiles/", site, "_classified-crowns"))) {
+          unlink(paste0("data/data_output/classified/model-classified/crown-shapefiles/", site, "_classified-crowns"), recursive = TRUE)
+        }
+        dir.create(paste0("data/data_output/classified/model-classified/crown-shapefiles/", site, "_classified-crowns"))
+      })
 
-ttops <- sf::st_read(paste0(current_dir, "/", current_site, "_ttops/", current_site, "_ttops.shp"))
-crowns <- 
-  sf::st_read(paste0(current_dir, "/", current_site, "_crowns/", current_site, "_crowns.shp")) %>% 
-  mutate(treeID = ttops$treeID)
+    classified_crowns <- vector(mode = "list", length = length(sites_to_process))
+    
+    for (i in seq_along(sites_to_process)) {
+      
+      site <- sites_to_process[i]
+      
+      ortho <- velox::velox(paste0("data/data_output/site_data/", site, "/3_dsm_ortho/2_mosaic/", site, "_x3_transparent_mosaic_group1.tif"))
+      names(ortho$rasterbands) <- c("r", "g", "b", "x")
+      ortho$rasterbands$rgi <- ortho$rasterbands$r / ortho$rasterbands$g
+      ortho$rasterbands$gbi <- ortho$rasterbands$g / ortho$rasterbands$b
+      ortho$nbands <- length(names(ortho$rasterbands))
+      
+      ttops <- sf::st_read(paste0("data/data_output/site_data/", site, "/", site, "_ttops/", site, "_ttops.shp"))
+      
+      crowns <- 
+        sf::st_read(paste0("data/data_output/site_data/", site, "/", site, "_crowns/", site, "_crowns.shp")) %>% 
+        dplyr::mutate(site = site) %>% 
+        dplyr::mutate(crs = st_crs(.)$proj4string) %>% 
+        dplyr::mutate(treeID = ttops$treeID) %>% 
+        dplyr::mutate(ttop_x = st_coordinates(ttops)[, "X"]) %>% 
+        dplyr::mutate(ttop_y = st_coordinates(ttops)[, "Y"]) %>% 
+        dplyr::mutate(object_ = 1:nrow(.))
+      
+      ortho_copy <- ortho$copy()
+      ortho_copy$crop(crowns)
+      
+      extracted_vals <- ortho_copy$extract(crowns, fun = function(x) mean(x, na.rm = TRUE))
+      colnames(extracted_vals) <- paste0(names(ortho_copy$rasterbands), "_mean")
+      extracted_vals <- as.data.frame(extracted_vals)
+      extracted_vals$treeID <- 1:nrow(extracted_vals)
+      
+      classified_crowns[[i]] <-
+        crowns %>% 
+        left_join(extracted_vals, by = "treeID") %>% 
+        dplyr::mutate(live_prob = plogis(predict(m1, newdata = ., type = "link")))
+      
+      sf::st_write(classified_crowns[[i]], paste0("data/data_output/classified/model-classified/crown-shapefiles/", site, "_classified-crowns/", site, "_classified-crowns.shp"))
+    }
+  }
+classified_crowns[[1]] <- st_read("data/data_output/classified/model-classified/crown-shapefiles/eldo_3k_1_classified-crowns/eldo_3k_1_classified-crowns.shp")
+classified_crowns[[2]]
 
-site_bounds <- 
-  sf::st_read(paste0(current_dir, "/", current_site, "_mission-footprint/", current_site, "_site-bounds.geoJSON")) %>% 
-  st_transform(st_crs(ttops))
+names(classified_crowns) <- sites_to_process
 
-ortho <- 
-  raster::brick(paste0(current_dir, 
-                       "/3_dsm_ortho/2_mosaic/", 
-                       current_site, 
-                       "_x3_transparent_mosaic_group1.tif")) %>% 
-  raster::crop(site_bounds)
-
-system.time({
-  live_or_dead <- 
-    cellnumbers(ortho[[1]], crowns) %>% 
-    dplyr::mutate(r = raster::extract(ortho[[1]], cell_)) %>% 
-    dplyr::mutate(g = raster::extract(ortho[[2]], cell_)) %>% 
-    dplyr::mutate(b = raster::extract(ortho[[3]], cell_)) %>% 
-    dplyr::mutate(rgi = r/g) %>% 
-    dplyr::mutate(rgvi = (r - g) / (r + g)) %>% 
-    dplyr::group_by(object_) %>% 
-    dplyr::summarize(r_mean = mean(r, na.rm = TRUE),
-                     g_mean = mean(g, na.rm = TRUE),
-                     b_mean = mean(b, na.rm = TRUE),
-                     rgi_mean = mean(rgi, na.rm = TRUE),
-                     rgvi_mean = mean(rgvi, na.rm = TRUE),
-                     r_hi = quantile(r, probs = 0.75, na.rm = TRUE),
-                     g_hi = quantile(g, probs = 0.75, na.rm = TRUE),
-                     b_hi = quantile(b, probs = 0.75, na.rm = TRUE),
-                     rgi_hi = quantile(rgi, probs = 0.75, na.rm = TRUE),
-                     rgvi_hi = quantile(rgvi, probs = 0.75, na.rm = TRUE)) %>% 
-    dplyr::rename(treeID = object_)
-})
-
-}
-
-crowns_class <- 
-  sf::st_read(paste0(current_dir, "/", current_site, "_live-or-dead/", current_site, "_live-or-dead.shp")) %>% 
-  mutate(treeID = ttops$treeID)
-
-ttops_extra <- 
-  ttops %>% 
-  left_join(dplyr::select(as.data.frame(crowns_class), -geometry), by = c("treeID", "layer", "height", "winRadius")) %>% 
-  left_join(live_or_dead, by = "treeID")
-
-pca_mean <- prcomp(live_or_dead[, 2:5], scale. = TRUE)
-ggplot(as.data.frame(pca_mean$x), aes(x = PC1, y = PC2)) + 
-  geom_point()
-
-pca_mean_not_scaled <- prcomp(live_or_dead[, 2:4])
-ggplot(as.data.frame(pca_mean_not_scaled$x), aes(x = PC1, y = PC2)) + 
-  geom_point()
-
-pca_hi <- prcomp(live_or_dead[, 6:9], scale. = TRUE)
-ggplot(as.data.frame(pca_hi$x), aes(x = PC1, y = PC2)) + 
-  geom_point()
-
-ggplot(live_or_dead, aes(x = r, y = g)) +
-  geom_point()
-
-ggplot(filter(test, !is.na(live)), aes(x = PC1, y = PC2, col = as.factor(live))) +
-  geom_point()
-
-pca_full <- prcomp(live_or_dead[, 2:7], scale = TRUE)
-
-ggplot(as.data.frame(pca_full$x), aes(x = PC1, y = PC2)) + 
-  geom_point()
-
-
-ggplot(as.data.frame(pca_full$x), aes(x = PC1, y = PC2, col = k$cluster)) + 
-  geom_point()
-
-classified <- 
-  ttops_extra %>%
-  filter(!is.na(live))
-
-m1 <- glm(live ~ rgi_mean, data = classified, family = "binomial")
-m2 <- glm(live ~ rgi_hi + rgi_mean, data = classified, family = "binomial")
-m3 <- glm(live ~ rgi_hi + r_mean + g_mean + b_mean, data = classified, family = "binomial")
-m4 <- glm(live ~ r_mean + g_mean + b_mean, data = classified, family = "binomial")
-m5 <- glm(live ~ r_mean + g_mean + b_mean + rgi_mean, data = classified, family = "binomial")
-AIC(m1, m2, m3, m4, m5)
-
-ttops_extra <-
-  ttops_extra %>% 
-  mutate(predicted_live = plogis(predict(m4, newdata = ., type = "link")))
-
-ggplot(ttops_extra, aes(col = predicted_live)) +
-  geom_sf() +
-  scale_color_viridis_c()
-
-hist(ttops_extra$predicted_live)
+i = 4
+ggplot(classified_crowns[[i]], aes(x = ttop_x, y = ttop_y, col = live_prob)) +
+  geom_point() +
+  scale_color_viridis_c(name = "Pr(live)", option = "E") +
+  coord_equal() +
+  theme_bw() +
+  xlab(label = "longitude") +
+  ylab(label = "latitude") +
+  ggtitle(names(classified_crowns[i])) +
+  theme(plot.title = element_text(margin = margin(b = 10)),
+        axis.title.x = element_text(margin = margin(t = 10, r = 0, b = 0, l = 0)),
+        axis.title.y = element_text(margin = margin(t = 0, r = 10, b = 0, l = 0)))
 
