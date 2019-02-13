06d09b4556dcee546e210eece79ad74549c71486
Michael Koontz <mikoontz@gmail.com>
2019-02-13 12:18:15 -0700

    complete the first full draft of code to generate DTM, classified point cloud (ground/nonground), and CHM from Pix4D outputs.

--- data/data_carpentry/create-canopy-height-models.R
+++ data/data_carpentry/create-canopy-height-models.R
@@ -1,10 +1,11 @@
-# Load libraries
+# Load packages
 library(RCSF)
 library(lidR)
 library(sf)
 library(tidyverse)
 library(purrr)
 library(raster)
+library(rgl)
 
 source("data/data_carpentry/make_processing-checklist.R")
 
@@ -15,56 +16,110 @@ unusable_sites <- c("eldo_4k_3", # too many blocks
                     "stan_5k_3", # too many blocks
                     "sequ_4k_2") # middle section flown on a separate day and the stitch looks terrible
 
-usable_sites <- 
+# These sites were processed with their X3 and RedEdge imagery combined so some of their
+# output products will be in a slightly different place in the project directory
+merged_sites <- c("eldo_3k_2",
+                  "eldo_3k_3",
+                  "eldo_4k_2")
+
+sites_to_process <- 
   sites_checklist %>% 
-  dplyr::filter(!(site %in% unusable_sites)) %>% 
+  dplyr::filter(!(site %in% unusable_sites)) %>%
+  dplyr::filter(!classified_point_cloud_check | !dtm_check | !chm_check) %>% 
   dplyr::pull(site)
 
-i = 1
-for (i in seq_along(usable_sites)) {
-  current_site <- usable_sites[i]
+
+(start <- Sys.time())
+
+for (i in seq_along(sites_to_process)) {
+  current_site <- sites_to_process[i]
+  
+  if (current_site %in% merged_sites) {
+    current_point_cloud <- lidR::readLAS(files = here::here(paste0("data/data_output/site_data/", current_site, "/", "2_densification/point_cloud/", current_site, "_densified_point_cloud.las")))
+  } else {
+    current_point_cloud <- lidR::readLAS(files = here::here(paste0("data/data_output/site_data/", current_site, "/", current_site, "_re/2_densification/point_cloud/", current_site, "_re_Green_densified_point_cloud.las")))
+  }
+  
+  
   # Wipe clean all of Pix4D classification
+  current_point_cloud@data[, Classification := 0L]
+
+  # Use the cloth simulation filter by Zhang et al. (2016) [http://www.mdpi.com/2072-4292/8/6/501/htm]
+  # implemented in the lidR package to classify points in the point cloud as ground or non-ground
   
-  # Get the file system right. Is it time to move all of the data_workign stuff to site_data??
+  # cloth resolution set to be ~5 times the average spacing between points
+  # when point cloud density is ~30 pts per m^2
+  current_point_cloud <- lidR::lasground(current_point_cloud, csf(sloop_smooth = TRUE, 
+                                                                  class_threshold = 0.25, 
+                                                                  cloth_resolution = 1.0,  
+                                                                  rigidness = 1, 
+                                                                  iterations = 500, 
+                                                                  time_step = 0.65))
   
-  las <- readLAS(files = here::here(paste0("data/data_working/", current_site, "/", current_site, stan_3k_2_re/2_densification/point_cloud/stan_3k_2_re_Green_densified_point_cloud.las")
-  las@data[, Classification := 0L]
   
+  # Plot the classification of the point cloud for inspection
+  plot(current_point_cloud, color = "Classification")
+  legend3d("topright", legend = current_site, bty = "n")
+  
+  
+  # Export the classified point cloud to disk so we can use the vegetation points for tree segmentation if
+  # we want
+  lidR::writeLAS(las= current_point_cloud, file = here::here(paste0("data/data_output/site_data/", current_site, "/", current_site, "_classified_point_cloud.las")))
+  
+  
+  # Create a 1m resolution digital terrain model using the classified ground points
+  # and interpolation between those ground points using the tin() function from lidR which 
+  # implements a Delaunay triangulation method
+  dtm <- lidR::grid_terrain(las = current_point_cloud,
+                            res = 1.0,
+                            algorithm = tin())
+  
+  
+  # Write the dtm file to disk so we can use it later.
+  # Note that all of these outputs generated using R get written to the same place, regardless of whether the
+  # output is derived from merged X3+RedEdge imagery versus just being derived from RedEdge imagery
+  raster::writeRaster(x = dtm, filename = here::here(paste0("data/data_output/site_data/", current_site, "/", current_site, "_dtm.tif")), overwrite = TRUE)
+  
+  # Import the digital surface model Pix4D output in order to generate a canopy height model
+  if (current_site %in% merged_sites) {
+    dsm <- raster::raster(x = here::here(paste0("data/data_output/site_data/", current_site, "/", "3_dsm_ortho/1_dsm/", current_site, "_dsm.tif")))
+  } else {
+    dsm <- raster::raster(x = here::here(paste0("data/data_output/site_data/", current_site, "/", current_site, "_re/3_dsm_ortho/1_dsm/", current_site, "_re_dsm.tif")))
+  }
+
+  
+  # Using bilinear interpolation to downsample the 1m resolution DTM to have the
+  # same resolution as the dsm (~5cm, but slightly different for each site)
+  dtm_resamp <- raster::resample(dtm, dsm, method = "bilinear")
+  
+  # The Canopy Height Model (chm) is the dsm (vegetation + ground) minus the dtm (ground)
+  # to give just the height of the vegetation.
+  chm <- dsm - dtm_resamp
+  
+  # Need to decide whether to do CHM smoothing now or just before the segmentation process.
+  # # Smooth out the chm and set any negative values to 0 (meaning "ground") following
+  # # advice from Zagalikis, Cameron, and Miller (2004) and references therein
+  # # More recently, a 3x3 pixel smoothing filter was specifically suggested as ideal
+  # # for sUAS derived chm by Mohan et al. (2017)
+  # chm_smooth <- raster::focal(chm, w = matrix(1, 3, 3), mean)
+  # chm_smooth[raster::getValues(chm_smooth) < 0] <- 0
+  
+  # Write the chm file to disk so we can use it later
+  # Note that all of these outputs generated using R get written to the same place, regardless of whether the
+  # output is derived from merged X3+RedEdge imagery versus just being derived from RedEdge imagery
+  raster::writeRaster(x = chm, filename = here::here(paste0("data/data_output/site_data/", current_site, "/", current_site, "_chm.tif")), overwrite = TRUE)
+
 }  
 
-(start <- Sys.time())
-
-# cloth resolution set to be ~5 times the average spacing between points
-# when point cloud density is ~30 pts per m^2
-
-las <- lasground(las, csf(sloop_smooth = TRUE, 
-                          class_threshold = 0.25, 
-                          cloth_resolution = 1.0,  
-                          rigidness = 1, 
-                          iterations = 500, 
-                          time_step = 0.65))
-
-plot(las, color = "Classification")
-
 (end <- Sys.time())
-print(end - start)
+(end - start)
 
-# DTM's partially using Pix4D point classification
-dtm1a = grid_terrain(las = las,
-                     res = 1.0,
-                     algorithm = knnidw(k = 100, p = 2))
-
-(start <- Sys.time())
-
-raster::plot(dtm1a)
-
-dtm1b = grid_terrain(las = las,
-                     res = 1.0,
-                     algorithm = tin())
-
-dtm <- raster::raster("data/data_output/site_data/stan_3k_2/stan_3k_2_re_1m-dtm.tif")
-dtm[dtm == -999] <- NA
-
-raster::plot(dtm)
-raster::plot(dtm1b)
-?tin
+# # Testing the snag detection algorithms. Might be another way to find dead trees.
+# bbpr_thresholds <- matrix(c(0.80, 0.80, 0.70,
+#                             0.85, 0.85, 0.60,
+#                             0.80, 0.80, 0.60,
+#                             0.90, 0.90, 0.55),
+#                           nrow =3, ncol = 4)
+# 
+# # Run snag classification and assign classes to each point
+# snags <- lassnags(las = current_point_cloud, algorithm = wing2015(neigh_radii = c(1.5, 1, 2), BBPRthrsh_mat = bbpr_thresholds))
