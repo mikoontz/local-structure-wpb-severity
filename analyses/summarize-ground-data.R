ddc6be2e43156ce6cc8bcb1166be6fcf275a88db
Michael Koontz <mikoontz@gmail.com>
2019-02-20 14:04:50 -0700

    split measure-ground-correspondence script into 3 separate scripts to cut down on clutter. Two additional scripts added are the set of helper functions that run the lidR functions and get some additional output for my purposes. Also a separate script that summarizes the ground data for comparison to the air data.
    Former-commit-id: 3ca823c8cf6f86b8bde01b151a493ea22a790269
    Former-commit-id: f20853a4c5bd3c5c259a703174b2d972c7059060
    Former-commit-id: 92af6aca08b0b531bf4313faa1e838bfdbe00f19
    Former-commit-id: 727f87c8291aeeefe3fb20baa642d970458359fd

---
+++ analyses/summarize-ground-data.R
@@ -0,0 +1,185 @@
+library(foreach)
+library(doParallel)
+library(sf)
+library(tidyverse)
+library(purrr)
+library(lidR)
+library(viridis)
+library(raster)
+library(ForestTools)
+library(gstat)
+library(nngeo)
+# devtools::install_github("Jean-Romain/lidRplugins")
+library(lidRplugins)
+library(furrr)
+
+source("analyses/vwf2.R")
+
+
+# begin main program ------------------------------------------------------
+
+
+# Pseudocode
+# Calculate the "validation metrics" for the ground data at the plots that are visible in the drone-derived orthophotos
+#
+# (Explanation: we define "validation metrics" to be things that we care about in the forest
+# By picking 2+ "validation metrics" that are somewhat non-correlated and then matching them to the same metrics calculated
+# using the treetop detection/crown segmentation algorithm, we can compare the quality of different algorithms.
+# The greater the combined correlation of the "validation metrics" between algorithm-derived metrics and ground data-derived
+# metrics, the better the algorithm.)
+#
+# Candidate "validation metrics" include:
+# tree density within a plot
+# live tree density
+# dead tree density
+# tree size distribution within a plot (25th and 75th percentile height; mean height)
+# proportion of live trees
+# Voronoi polygon areas might not work due to edge effects, but let's try mean distance to 3 nearest trees
+
+# Establish a method for detecting tree tops and segmenting crowns
+# Work through all of the remotely-visible plots at each site (144 in total) applying the ttops and crown segmentation approach
+
+# These sites were processed with their X3 and RedEdge imagery combined so some of their
+# output products will be in a slightly different place in the project directory
+merged_sites <- c("eldo_3k_2",
+                  "eldo_3k_3",
+                  "eldo_4k_2")
+
+unusable_sites <- c("eldo_4k_3", # too many blocks
+                    "stan_4k_3", # too many blocks
+                    "stan_5k_3", # too many blocks
+                    "sequ_4k_2") # middle section flown on a separate day and the stitch looks terrible
+
+# First get the formatted ground data; the object is called `d`
+source("data/data_carpentry/format_ground-data.R")
+source("data/data_carpentry/make_processing-checklist.R")
+
+# This is where I can put in sites that need their processing redone. An empty 
+# string means that no already-processed site output will be overwritten
+# (but sites that have yet to be processed will still have their processing done)
+
+sites_to_overwrite <- ""
+sites_checklist$overwrite <- FALSE
+
+sites_checklist[sites_checklist$site %in% sites_to_overwrite, "overwrite"] <- TRUE
+
+sites_to_process <- 
+  sites_checklist %>% 
+  dplyr::filter(!(site %in% unusable_sites)) %>%
+  dplyr::filter(overwrite | plot_remote_data_check) %>% 
+  dplyr::select(site) %>% 
+  dplyr::pull()
+
+ground_trees <- 
+  sites_to_process %>% 
+  map(.f = function(current_site) {
+    
+    if (current_site %in% merged_sites) {
+      current_site_plot_locations <- 
+        sf::st_read(paste0("data/data_output/site_data/", current_site, "/", current_site, "_plot-locations/", current_site, "_plot-locations.shp")) %>% 
+        mutate(plot = paste(current_site, id, sep = "_")) %>%
+        dplyr::arrange(id) %>% 
+        st_zm()
+    } else {
+      current_site_plot_locations <- 
+        sf::st_read(paste0("data/data_output/site_data/", current_site, "/", current_site, "_plot-locations_re/", current_site, "_plot-locations_re.shp")) %>% 
+        mutate(plot = paste(current_site, id, sep = "_")) %>%
+        dplyr::arrange(id) %>% 
+        st_zm()
+    }
+    
+    current_plot_ground_trees <- 
+      d %>% 
+      filter(site == current_site) %>% 
+      left_join(current_site_plot_locations, by = "plot") %>% 
+      dplyr::mutate(delta_x = cospi((1 / 2) - (azm / 180)) * dist) %>% 
+      dplyr::mutate(delta_y = sinpi((1 / 2) - (azm / 180)) * dist) %>%
+      st_as_sf() %>%
+      dplyr::mutate(x = st_coordinates(.)[, "X"],
+                    y = st_coordinates(.)[, "Y"]) %>% 
+      as.data.frame() %>% 
+      dplyr::mutate(new_x = x + delta_x,
+                    new_y = y + delta_y) %>% 
+      filter(!is.na(x)) %>% 
+      st_as_sf(coords = c("new_x", "new_y")) %>% 
+      st_set_crs(st_crs(current_site_plot_locations))
+    
+    nn <- 
+      st_nn(current_plot_ground_trees, current_plot_ground_trees, k = min(c(4, nrow(current_plot_ground_trees))), returnDist = TRUE, sparse = FALSE, progress = FALSE)$dist %>% 
+      as.data.frame()
+    
+    nn <- nn %>% dplyr::select(-1)
+    
+    if (ncol(nn) == 1) {nn <- nn %>% rename(nn1 = V2) %>% mutate(nn2 = NA, nn3 = NA)} else
+      if (ncol(nn) == 2) {nn <- nn %>% rename(nn1 = V2, nn2 = V3) %>% mutate(nn3 = NA)} else
+        if (ncol(nn) == 3) {nn <- nn %>% rename(nn1 = V2, nn2 = V3, nn3 = V4) }
+    
+    current_plot_ground_trees <- 
+      current_plot_ground_trees %>% 
+      bind_cols(nn)  %>% 
+      st_transform(4326)
+  }) %>%
+  do.call("rbind", .)
+
+# Use the plot radius to determine the area of the plot and thus the tree density
+plot_radius <- sqrt((66*66) / pi) * 12 *2.54 / 100
+
+# Get the validation metrics for the ground plot trees (that hadn't fallen by the time of the flights)
+ground_tree_summary <-
+  ground_trees %>% 
+  dplyr::filter(is.na(year_fall)) %>% 
+  dplyr::group_by(plot) %>% 
+  dplyr::summarize(ttops_method = "ground",
+                   total_tree_count = n(),
+                   live_tree_count = sum(live),
+                   dead_tree_count = n() - sum(live),
+                   total_density_tph = (n() * 10000) / (pi * plot_radius ^ 2),
+                   live_density_tph = (sum(live) * 10000) / (pi * plot_radius ^ 2),
+                   dead_density_tph = ((n() - sum(live)) * 10000) / (pi * plot_radius ^ 2),
+                   live_proportion = sum(live) / n(),
+                   height_lwr_25 = quantile(height, prob = 0.25),
+                   height_mean = mean(height),
+                   height_upr_25 = quantile(height, prob = 0.75),
+                   nn_1_mean = mean(nn1),
+                   nn_2_mean = mean(nn2),
+                   nn_3_mean = mean(nn3),
+                   tree_count_above_15m = sum(height >= 15),
+                   tree_count_below_15m = sum(height < 15)) %>% 
+  as.data.frame() %>% 
+  dplyr::select(-geometry) %>% 
+  dplyr::mutate(elapsed_time = NA) %>% 
+  dplyr::select(plot, ttops_method, elapsed_time, everything())
+
+ground_trees %>% 
+  group_by(live) %>% 
+  summarize(min = min(height),
+            `10` = quantile(height, probs = 0.10),
+            `20` = quantile(height, probs = 0.20),
+            `30` = quantile(height, probs = 0.30),
+            `40` = quantile(height, probs = 0.40),
+            mean = mean(height),
+            `50` = quantile(height, probs = 0.50),
+            `60` = quantile(height, probs = 0.60),
+            `70` = quantile(height, probs = 0.70),
+            `80` = quantile(height, probs = 0.80),
+            `90` = quantile(height, probs = 0.90),
+            max = max(height),
+            ecdf_05 = ecdf(height)(5),
+            ecdf_10 = ecdf(height)(10),
+            ecdf_15 = ecdf(height)(15),
+            ecdf_20 = ecdf(height)(20),
+            ecdf_10_dbh = ecdf(dbh)(10),
+            ecdf_15_dbh = ecdf(dbh)(15),
+            ecdf_20_dbh = ecdf(dbh)(20),
+            ecdf_25_dbh = ecdf(dbh)(25))
+
+ggplot(ground_trees, aes(x = dbh, y = height, col = as.factor(live))) +
+  geom_point() +
+  geom_smooth(method = "lm")
+
+# Now we have a summary of what all of the ground plots looks like with respsect
+# to a few of these plot-level characteristics.
+# Now we need to employ a bunch of segmentation algorithms to find out which
+# one matches best to the ground data.
+
+readr::write_csv(ground_tree_summary, here::here("analyses/analyses_output/ground-tree-summary.csv"))
