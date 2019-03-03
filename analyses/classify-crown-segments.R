41b92783f3d09e3e44eb8e7709d0eba9a40194ce
Michael Koontz <mikoontz@gmail.com>
2019-03-03 16:29:53 -0700

    build simple allometric scaling equations from the ground data to convert height to dbh

--- analyses/classify-crown-segments.R
+++ analyses/classify-crown-segments.R
@@ -1,6 +1,5 @@
 library(sf)
 library(raster)
-library(tabularaster)
 library(tidyverse)
 library(viridis)
 library(purrr)
@@ -10,36 +9,55 @@ library(viridis)
 # Source in the classifier code.
 # The live/dead classifier is an R object called `live_or_dead_fit`
 # The species classifier is an R object called `rdafit`
-
 source(here::here("analyses/build-crown-classifier.R"))
 live_or_dead_fit
 rdaFit
 
-# crowns_with_reflectance <- readr::read_csv(file = here::here("data/data_output/classified/model-classified/crowns-with-reflectance_all.csv"))
+# Source in the allometric scaling models based on the ground data
+# object is called `allometry_models`
+source(here::here("analyses/allometric-scaling-models.R"))
+allometry_models
+
+allometry_models %>% filter(species == "pipo") %>% pull(model) %>% magrittr::extract2(1) %>% predict(newdata = data.frame(height = 20))
+
 crowns_with_reflectance <- readr::read_csv(file = here::here("data/data_output/classified/model-classified/crowns-with-reflectance_35m-buffer.csv"))
 
 classified_trees  <-
   crowns_with_reflectance %>% 
   dplyr::mutate(live = live_or_dead_fit$levels[predict(live_or_dead_fit, newdata = .)]) %>% 
   dplyr::mutate(live = as.numeric(as.character(live))) %>%
-  dplyr::mutate(species = ifelse(live == 1, yes = rdaFit$levels[predict(rdaFit, newdata = .)], no = NA))
+  dplyr::mutate(species = ifelse(live == 1, yes = rdaFit$levels[predict(rdaFit, newdata = .)], no = NA)) 
 
-classified_trees_3310 <-
+classified_and_allometried_trees <-
   classified_trees %>% 
+  dplyr::left_join(allometry_models, by = "species") %>% 
+  dplyr::mutate(model = ifelse(test = live == 0, 
+                               yes = allometry_models %>% 
+                                 dplyr::filter(species == "pipo") %>% 
+                                 pull(model), 
+                               no = model)) %>% 
+  dplyr::do(modelr::add_predictions(., model = first(.$model), var = "estimated_dbh")) %>% 
+  dplyr::select(-model) %>% 
+  dplyr::mutate(estimated_ba = (estimated_dbh / 2)^2 * pi)
+
+classified_trees_4326 <-
+  classified_and_allometried_trees %>% 
   split(f = .$crs) %>% 
   lapply(FUN = function(trees) {
     current_crs <- unique(trees$crs)
     
-    trees3310 <-
+    trees4326 <-
       trees %>% 
-      st_as_sf(coords = c("x", "y"), crs = current_crs, remove = TRUE) %>% 
-      st_transform(3310) %>% 
+      st_as_sf(coords = c("x", "y"), crs = current_crs, remove = FALSE) %>% 
+      st_transform(4326) %>% 
+      dplyr::rename(local_crs = crs,
+                    local_x = x,
+                    local_y = y) %>% 
       tidyr::separate(col = treeID, into = c("forest", "elev", "rep", "id"), sep = "_", remove = FALSE) %>% 
       dplyr::mutate(site = paste(forest, elev, rep, sep = "_")) %>% 
-      dplyr::select(-crs, -id) %>% 
-      dplyr::select(treeID, site, forest, elev, rep, live, species, everything())
+      dplyr::select(-id) %>% 
+      dplyr::select(treeID, site, forest, elev, rep, live, species, height, estimated_dbh, estimated_ba, everything())
   }) %>% 
   do.call("rbind", .)
 
-
-sf::st_write(classified_trees_3310, dsn = here::here("analyses/analyses_output/classified-trees/classified-trees.shp"))
+sf::st_write(classified_trees_4326, dsn = here::here("analyses/analyses_output/classified-trees.geojson"), delete_dsn = TRUE)
