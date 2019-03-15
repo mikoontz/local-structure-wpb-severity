4aaf86ada9e3984d608ca4fa1b72ada1ac547e6e
Michael Koontz <mikoontz@gmail.com>
2019-03-15 08:25:46 -0600

    testing a brms implementation of approximate gaussian processes using varying degrees of data subsampling
    Former-commit-id: 1424d43ba2b610f1e2c8a0c8cb6b66678ee8434a
    Former-commit-id: b78b40a26705b2b080aa3671bd0b35e3d5b8b8bb
    Former-commit-id: 2cf7d8b665359220ede1bead1657055b16c36b16
    Former-commit-id: bc6d275f203415ec27734bc8bd58bfb58843a20a

--- analyses/effect-of-local-structure-on-wpb-severity.R
+++ analyses/effect-of-local-structure-on-wpb-severity.R
@@ -5,6 +5,7 @@ library(raster)
 library(sf)
 library(here)
 library(tictoc)
+library(mgcv)
 library(brms)
 library(lme4)
 library(effects)
@@ -41,62 +42,97 @@ analysis_df <-
                 pipo_and_dead_sdi_ac_s = scale(pipo_and_dead_sdi_ac, center = center_param, scale = scale_param),
                 overall_sdi_ac_s = scale(overall_sdi_ac, center = center_param, scale = scale_param))
 
+set.seed(0314)
 adf <-
   analysis_df %>% 
   dplyr::filter(pipo_and_dead_count != 0) %>% 
-  dplyr::mutate(site = as.factor(site))
+  dplyr::mutate(site = as.factor(site)) %>% 
+  dplyr::group_by(site) %>% 
+  dplyr::sample_n(200)
 
-tic()
-fm1_mgcv <- bam(cbind(dead_count, pipo_count) ~ 
+
+# n = 10: 217 seconds (0.06 hours)
+# n = 50: 1454 seconds (0.4 hours)
+# n = 100: 1.6 hours
+# n = 200 = 8.9 hours
+
+# tic()
+# fm1_mgcv <- bam(cbind(dead_count, pipo_count) ~ 
+#                   site_cwd_zscore*pipo_and_dead_tpha_s*pipo_and_dead_qmd_s +
+#                   site_cwd_zscore*overall_tpha_s*overall_qmd_s +
+#                   s(x, y, by = site, bs = "gp", k = 5), 
+#                 data = adf, 
+#                 family = binomial(link = "logit"))
+# toc()
+# summary(fm1_mgcv)
+# plot(fm1_mgcv)
+
+# tic()
+# fm2_mgcv <- bam(cbind(dead_count, pipo_count) ~ 
+#                   site_cwd_zscore*pipo_and_dead_tpha_s*pipo_and_dead_qmd_s +
+#                   site_cwd_zscore*overall_tpha_s*overall_qmd_s +
+#                   s(x, y, by = site, bs = "gp", k = 10, m = c(3, 5)), 
+#                 data = adf, 
+#                 family = binomial(link = "logit"))
+# toc()
+# summary(fm2_mgcv)
+
+
+# 
+# tic()
+# fm3_mgcv <- bam(cbind(dead_count, pipo_count) ~ 
+#                   local_cwd_zscore*pipo_and_dead_tpha_s*pipo_and_dead_qmd_s +
+#                   local_cwd_zscore*overall_tpha_s*overall_qmd_s +
+#                   s(x, y, by = site, k = 10), 
+#                 data = adf, 
+#                 family = binomial(link = "logit"))
+# toc()
+# summary(fm3_mgcv)
+# 
+# 
+# tic()
+# fm4_mgcv <- bam(cbind(dead_count, pipo_count) ~ 
+#                   site_cwd_zscore*pipo_and_dead_tpha_s*pipo_and_dead_qmd_s +
+#                   site_cwd_zscore*overall_tpha_s*overall_qmd_s +
+#                   s(x, y, by = site, k = 10), 
+#                 data = adf, 
+#                 family = binomial(link = "logit"))
+# toc()
+# summary(fm4_mgcv)
+
+# library(parallel)  
+# nc <- 6   ## cluster size, set for example portability
+# cl <- makeCluster(nc) 
+# tic()
+# fm5_mgcv <- bam(cbind(dead_count, pipo_count) ~ 
+#                   site_cwd_zscore*pipo_and_dead_tpha_s*pipo_and_dead_qmd_s +
+#                   site_cwd_zscore*overall_tpha_s*overall_qmd_s +
+#                   te(x, y, by = site), 
+#                 data = adf, 
+#                 family = binomial(link = "logit"))
+# # ,
+# #                 cluster = cl)
+# toc()
+# summary(fm5_mgcv)
+# stopCluster(cl)
+
+(start <- Sys.time())
+fm1_brms <- brm(dead_count | trials(pipo_and_dead_count) ~ 
                   site_cwd_zscore*pipo_and_dead_tpha_s*pipo_and_dead_qmd_s +
                   site_cwd_zscore*overall_tpha_s*overall_qmd_s +
-                  s(x, y, by = site, bs = "gp", k = 10), 
-                data = adf, 
-                family = binomial(link = "logit"))
-toc()
-summary(fm1_mgcv)
-plot(fm1_mgcv)
+                  gp(x, y, by = site, k = 3, scale = FALSE),
+                data = adf,
+                family = binomial(link = "logit"),
+                chains = 4,
+                cores = 4,
+                iter = 2500,
+                control = list(adapt_delta = 0.95))
+summary(fm1_brms)
+(elapsed <- Sys.time() - start)
 
-tic()
-fm2_mgcv <- bam(cbind(dead_count, pipo_count) ~ 
-                  site_cwd_zscore*pipo_and_dead_tpha_s*pipo_and_dead_qmd_s +
-                  site_cwd_zscore*overall_tpha_s*overall_qmd_s +
-                  s(x, y, by = site, bs = "gp", k = 10, m = c(3, 5)), 
-                data = adf, 
-                family = binomial(link = "logit"))
-toc()
-summary(fm2_mgcv)
+prior_summary(fm1_brms)
 
-
-
-tic()
-fm3_mgcv <- bam(cbind(dead_count, pipo_count) ~ 
-                  local_cwd_zscore*pipo_and_dead_tpha_s*pipo_and_dead_qmd_s +
-                  local_cwd_zscore*overall_tpha_s*overall_qmd_s +
-                  s(x, y, by = site, k = 10), 
-                data = adf, 
-                family = binomial(link = "logit"))
-toc()
-summary(fm3_mgcv)
-
-
-tic()
-fm4_mgcv <- bam(cbind(dead_count, pipo_count) ~ 
-                  site_cwd_zscore*pipo_and_dead_tpha_s*pipo_and_dead_qmd_s +
-                  site_cwd_zscore*overall_tpha_s*overall_qmd_s +
-                  s(x, y, by = site, k = 10), 
-                data = adf, 
-                family = binomial(link = "logit"))
-toc()
-summary(fm4_mgcv)
-
-
-
-
-
-
-
-
+saveRDS(fm1_brms, file = here::here("analyses/analyses_output/fitted-model_site-cwdZscore_pipo-tpha-qmd_overall-tpha-qmd_gp-per-site_200-samples.rds"))
 
 # if(!file.exists(here::here("analyses/analyses_output/fitted-model_cwdZscore_pipo-count-ba_total-count-ba_uniqueCellID.rds"))) {
 #   # tic()
