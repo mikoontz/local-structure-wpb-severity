408da49073ac60835244f95a267235e1c10f7f9d
Michael Koontz <mikoontz@gmail.com>
2019-03-29 21:21:15 -0600

    multipanel plot components
    Former-commit-id: 1605a7ec11a97df5176270d636d944e9fa384627
    Former-commit-id: 30f4a6782bf45bd0bf07af4a0c289271009b94e2
    Former-commit-id: 1a67173431ddad4a24d25e3c91290d78195c85ef
    Former-commit-id: af374ac8fd4e25a0836de5cad0783bf79cab0adc

--- figures/figure_carpentry/create-processing-figures-re-multipanel.R
+++ figures/figure_carpentry/create-processing-figures-re-multipanel.R
@@ -6,44 +6,168 @@
 # Tree detection
 # Segmentation
 # Classification (live/dead; species)
-# Two columns, 4 rows I think.
+# rasterized?
+
+# 3x3
+
+# DSM; Point Cloud; DTM
+# CHM; ttops; crowns
+# live/dead; species; rasterized
+
+# alternatively, for rasterized
+# rasterized live; rasterized dead
+# rasterized host; rasterized non-host
+# rasterized host qmd; rasterized non-host qmd
 
 library(tidyverse)
 library(raster)
 library(viridis)
 library(lidR)
+library(sf)
+library(png)
+library(magick)
+library(cowplot)
+library(here)
 
 source("data/data_carpentry/make-processing-checklist.R")
+cc <- sf::st_read(here::here("analyses/analyses_output/classified-trees.geojson"))
+
+# These sites were processed with their X3 and RedEdge imagery combined so some of their
+# output products will be in a slightly different place in the project directory
+merged_sites <- c("eldo_3k_2",
+                  "eldo_3k_3",
+                  "eldo_4k_2")
+
 example_site <- 
   sites_checklist %>% 
   dplyr::filter(rasterized_trees_check) %>% 
-  slice(1) %>% 
+  slice(3) %>% 
   pull(site)
-"data/data_output/site_data/eldo_3k_1/"
-dsm <- raster::raster(paste0("data/data_output/site_data/", example_site, "))
+
+# The Digital Surface Model (dsm) is the ~5cm resolution raster representing
+# the surface (ground + objects on top) that the drone flew over
+if (example_site %in% merged_sites) {
+  dsm <- raster::raster(x = here::here(paste0("data/data_output/site_data/", example_site, "/", "3_dsm_ortho/1_dsm/", example_site, "_dsm.tif")))
+} else {
+  dsm <- raster::raster(x = here::here(paste0("data/data_output/site_data/", example_site, "/", example_site, "_re/3_dsm_ortho/1_dsm/", example_site, "_re_dsm.tif")))
+}
+ 
+# if (example_site %in% merged_sites) {
+#   point_cloud <- lidR::readLAS(files = here::here(paste0("data/data_output/site_data/", example_site, "/", "2_densification/point_cloud/", example_site, "_densified_point_cloud.las")))
+# } else {
+#   point_cloud <- lidR::readLAS(files = here::here(paste0("data/data_output/site_data/", example_site, "/", example_site, "_re/2_densification/point_cloud/", example_site, "_re_Green_densified_point_cloud.las")))
+# }
+# plot(point_cloud, colorPalette = viridis(100))
+
+dtm <- raster::raster(paste0("data/data_output/site_data/", example_site, "/", example_site, "_dtm.tif"))
+current_chm_rough <- raster::raster(paste0("data/data_output/site_data/", example_site, "/", example_site, "_chm.tif"))
+chm <- raster::focal(current_chm_rough, w = matrix(1/9, nrow = 3, ncol = 3))
+# current_chm <- current_chm_rough
+chm[raster::getValues(chm) < 0] <- 0
 
 
-dtm <- raster::raster(paste0("data/data_output/site_data/", site, "/", site, "_2m-dtm.tif"))
-
-site_bounds <- 
-  sf::st_read(paste0("data/data_output/site_data/", site, "/", site, "_mission-footprint/", site, "_site-bounds.geoJSON")) %>% 
-  sf::st_transform(proj4string(dsm))
-
-dsm <- 
+dsm_gg <-
   dsm %>% 
-  raster::crop(site_bounds)
+  as.data.frame(xy = TRUE) %>% 
+  setNames(c("x", "y", "z")) %>% 
+  ggplot(mapping = aes(x = x, y = y, fill = z)) +
+  geom_raster() +
+  scale_fill_viridis_c(na.value = "white") +
+  coord_equal() +
+  labs(x = "longitude", y = "latitude", fill = "m.s.l. (m)") +
+  theme_bw()
 
-dtm <-
+dtm_gg <-
   dtm %>% 
-  raster::crop(site_bounds) %>% 
-  raster::resample(dsm)
+  as.data.frame(xy = TRUE) %>% 
+  setNames(c("x", "y", "z")) %>% 
+  ggplot(mapping = aes(x = x, y = y, fill = z)) +
+  geom_raster() +
+  scale_fill_viridis_c(na.value = "white") +
+  coord_equal() +
+  labs(x = "longitude", y = "latitude", fill = "m.s.l. (m)") +
+  theme_bw()
 
-chm <- dsm - dtm
-chm <-
-  raster::focal(chm, w = matrix(1, 3, 3), mean)
-chm[chm < 0] <- 0
+chm_gg <-
+  chm %>% 
+  as.data.frame(xy = TRUE) %>% 
+  setNames(c("x", "y", "z")) %>% 
+  ggplot(mapping = aes(x = x, y = y, fill = z)) +
+  geom_raster() +
+  scale_fill_viridis_c(na.value = "white") +
+  coord_equal() +
+  labs(x = "longitude", y = "latitude", fill = "a.g.l. (m)") +
+  theme_bw()
 
-ttops <- sf::st_read(paste0("data/data_output/site_data/", site, "/", site, "_ttops/", site, "_ttops.shp"))
-crowns <- sf::st_read(paste0("data/data_output/site_data/", site, "/", site, "_crowns/", site, "_crowns.shp"))
 
-cc <- sf::st_read(paste0("data/data_output/classified/model-classified/crown-shapefiles/", site, "_classified-crowns/", site, "_classified-crowns.shp"))
+ttops <- 
+  sf::st_read(paste0("data/data_output/site_data/", example_site, "/", example_site, "_ttops/", example_site, "_ttops.shp")) %>% 
+  dplyr::mutate(x = st_coordinates(.)[,1],
+                y = st_coordinates(.)[,2])
+
+crowns <- sf::st_read(paste0("data/data_output/site_data/", example_site, "/", example_site, "_crowns/", example_site, "_crowns.shp"))
+
+ttops_gg <-
+  ggplot(ttops, aes(x = x, y = y)) +
+  geom_point(cex = 0.2) +
+  coord_equal() +
+  labs(x = "longitude", y = "latitude") +
+  theme_bw()
+
+crowns_gg <-
+  ggplot(crowns) +
+  geom_sf() +
+  coord_sf() +
+  labs(x = "longitude", y = "latitude") +
+  theme_bw()
+
+site_cc <- 
+  cc %>% 
+  dplyr::filter(site == example_site) %>% 
+  sf::st_transform(3310) %>% 
+  dplyr::mutate(live = ifelse(live == 1, yes = "live", no = "dead")) %>% 
+  dplyr::mutate(host = ifelse((live == "live" & species == "pipo") | live == "dead",
+                              yes = "host",
+                              no = "non-host"))
+
+live_dead_gg <-
+  ggplot(site_cc, aes(x = local_x, y = local_y, color = live)) +
+  geom_point(cex = 0.75) +
+  coord_sf() +
+  labs(x = "longitude", y = "latitude", color = "Status") +
+  theme_bw() +
+  scale_color_viridis_d()
+
+host_gg <-
+  ggplot(site_cc, aes(x = local_x, y = local_y, color = host)) +
+  geom_point() +
+  coord_sf() +
+  labs(x = "longitude", y = "latitude", color = "Species") +
+  theme_bw() +
+  scale_color_viridis_d()
+
+
+ggsave(plot = dsm_gg, filename = "figures/eldo_3k_3_dsm.png")
+ggsave(plot = dtm_gg, filename = "figures/eldo_3k_3_dtm.png")
+ggsave(plot = chm_gg, filename = "figures/eldo_3k_3_chm.png")
+ggsave(plot = ttops_gg, filename = "figures/eldo_3k_3_ttops.png")
+ggsave(plot = crowns_gg, filename = "figures/eldo_3k_3_crowns.png")
+ggsave(plot = live_dead_gg, filename = "figures/eldo_3k_3_live_dead.png")
+ggsave(plot = host_gg, filename = "figures/eldo_3k_3_host_nonhost.png")
+
+point_cloud_png <- ggdraw() + draw_image("figures/eldo_3k_3_point_cloud.png")
+dsm_png <- ggdraw() + draw_image("figures/eldo_3k_3_dsm.png")
+dtm_png <- ggdraw() + draw_image("figures/eldo_3k_3_dtm.png")
+chm_png <- ggdraw() + draw_image("figures/eldo_3k_3_chm.png")
+ttops_png <- ggdraw() + draw_image("figures/eldo_3k_3_ttops.png")
+crowns_png <- ggdraw() + draw_image("figures/eldo_3k_3_crowns.png")
+live_dead_png <- ggdraw() + draw_image("figures/eldo_3k_3_live_dead.png")
+host_nonhost_png <- ggdraw() + draw_image("figures/eldo_3k_3_host_nonhost.png")
+
+panel_plot <- plot_grid(dsm_png, point_cloud_png,
+                        dtm_png, chm_png,
+                        ttops_png, crowns_png,
+                        live_dead_gg, host_nonhost_png,
+                        nrow = 4, ncol = 2)
+
+panel_plot
