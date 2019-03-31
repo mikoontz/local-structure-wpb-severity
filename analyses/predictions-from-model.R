07854cb6e283dc3aaa64941241110f7f4603e75e
Michael Koontz <mikoontz@gmail.com>
2019-03-31 10:18:41 -0600

    create and incorporate plots of model predictions and model estimates of effect sizes
    Former-commit-id: 8a2464d05519514e07f9c66fce7c852056b08554
    Former-commit-id: 2046d32678262f269a3f350c30d49762f20792e7
    Former-commit-id: 050bfba2c1e44a50609f96cc8496506ed7f759a1
    Former-commit-id: 47edb9bfe06b1c491ffe8e39e40e8c9a4dce8d78

---
+++ analyses/predictions-from-model.R
@@ -0,0 +1,88 @@
+# Purpose: Use the fitted model to get model predictions for a range of newdata
+# Will be useful for plotting, and "zeroing out" the spatial Gaussian Process 
+# in order to focus on the fixed effects of the forest structure/environment
+# covariates
+
+library(tidyverse)
+library(brms)
+
+fm1 <- readRDS("analyses/analyses_output/fitted-model_zibinomial_site-cwdZscore_pipo-tpha-qmd_overall-tpha-qmd_exact-gp-per-site_200-samples.rds")
+
+step_size <- 0.1
+
+newdata <- 
+  expand.grid(Intercept = 1,
+              site_cwd_zscore = -1:1,
+              pipo_and_dead_tpha_s = seq(-1, 1, by = step_size),
+              pipo_and_dead_qmd_s = seq(-1, 1, by = step_size),
+              overall_tpha_s = seq(-1, 1, by = step_size),
+              overall_qmd_s = seq(-1, 1, by = step_size)) %>% 
+  as_tibble() %>% 
+  dplyr::mutate(`site_cwd_zscore:pipo_and_dead_tpha_s` = site_cwd_zscore * pipo_and_dead_tpha_s,
+                `site_cwd_zscore:pipo_and_dead_qmd_s` = site_cwd_zscore * pipo_and_dead_qmd_s,
+                `pipo_and_dead_tpha_s:pipo_and_dead_qmd_s` = pipo_and_dead_tpha_s * pipo_and_dead_qmd_s,
+                `site_cwd_zscore:overall_tpha_s` = site_cwd_zscore * overall_tpha_s,
+                `site_cwd_zscore:overall_qmd_s` = site_cwd_zscore * overall_qmd_s,
+                `overall_tpha_s:overall_qmd_s` = overall_tpha_s * overall_qmd_s,
+                `site_cwd_zscore:pipo_and_dead_tpha_s:pipo_and_dead_qmd_s` = site_cwd_zscore * pipo_and_dead_tpha_s * pipo_and_dead_qmd_s,
+                `site_cwd_zscore:overall_tpha_s:overall_qmd_s` = site_cwd_zscore * overall_tpha_s * overall_qmd_s) %>% 
+  dplyr::select(Intercept,
+                site_cwd_zscore,
+                pipo_and_dead_tpha_s,
+                pipo_and_dead_qmd_s,
+                overall_tpha_s,
+                overall_qmd_s,
+                `site_cwd_zscore:pipo_and_dead_tpha_s`,
+                `site_cwd_zscore:pipo_and_dead_qmd_s`,
+                `pipo_and_dead_tpha_s:pipo_and_dead_qmd_s`,
+                `site_cwd_zscore:overall_tpha_s`,
+                `site_cwd_zscore:overall_qmd_s`,
+                `overall_tpha_s:overall_qmd_s`,
+                `site_cwd_zscore:pipo_and_dead_tpha_s:pipo_and_dead_qmd_s`,
+                `site_cwd_zscore:overall_tpha_s:overall_qmd_s`)
+
+write_csv(newdata, path = "analyses/analyses_output/newdata-for-model-predictions.csv")
+
+samps <- posterior_samples(fm1)[, 1:14]
+
+(start <- Sys.time())
+lwr <- purrr::map_dbl(1:nrow(newdata), .f = function(row) {quantile(plogis(as.matrix(samps) %*% t(newdata[row, ])), prob = 0.025)})
+(Sys.time() - start)
+
+(start <- Sys.time())
+est_med <- purrr::map_dbl(1:nrow(newdata), .f = function(row) {median(plogis(as.matrix(samps) %*% t(newdata[row, ])))})
+(Sys.time() - start)
+
+(start <- Sys.time())
+est_mn <- purrr::map_dbl(1:nrow(newdata), .f = function(row) {mean(plogis(as.matrix(samps) %*% t(newdata[row, ])))})
+(Sys.time() - start)
+
+(start <- Sys.time())
+est_sd <- purrr::map_dbl(1:nrow(newdata), .f = function(row) {sd(plogis(as.matrix(samps) %*% t(newdata[row, ])))})
+(Sys.time() - start)
+
+(start <- Sys.time())
+upr <- purrr::map_dbl(1:nrow(newdata), .f = function(row) {quantile(plogis(as.matrix(samps) %*% t(newdata[row, ])), prob = 0.975)})
+(Sys.time() - start)
+
+fitted <- newdata
+fitted$lwr <- lwr
+fitted$est_med <- est_med
+fitted$est_mn <- est_mn
+fitted$upr <- upr
+fitted$est_sd <- est_sd
+
+object.size(fitted)
+head(fitted)
+
+fitted_compact <- 
+  fitted %>% 
+  dplyr::select(site_cwd_zscore,
+                pipo_and_dead_tpha_s,
+                pipo_and_dead_qmd_s,
+                overall_tpha_s,
+                overall_qmd_s,
+                lwr, est_med, est_mn, upr, est_sd)
+
+object.size(fitted_compact)
+write_csv(fitted_compact, path = "analyses/analyses_output/model-predictions.csv")
---
+++ figures/figure_carpentry/effects-of-local-structure-on-wpb-severity.R
@@ -0,0 +1,102 @@
+# Purpose: visualize the results of the primary analysis
+# Triptych of low, average, and high CWD in different facets
+# pondo qmd vs. pondo density with color of point representing probability of
+# pondo mortality
+# all tree qmd vs. all tree density with color of point representing Pr(pipo mortality)
+
+library(tidyverse)
+library(brms)
+library(tidybayes)
+
+fm1 <- readRDS("analyses/analyses_output/fitted-model_zibinomial_site-cwdZscore_pipo-tpha-qmd_overall-tpha-qmd_exact-gp-per-site_200-samples.rds")
+
+newdata <- read_csv("analyses/analyses_output/newdata-for-model-predictions.csv")
+fitted_compact <- read_csv("analyses/analyses_output/model-predictions.csv")
+
+step_size <- 0.1
+
+fitted_just_estimates <- newdata
+fitted_just_estimates$prob_pipo_mortality <- plogis(as.matrix(newdata) %*% fixef(fm1)[, "Estimate"])
+
+fitted_subset <-
+  fitted_just_estimates %>% 
+  dplyr::filter(overall_tpha_s == 0,
+                overall_qmd_s == 0) %>% 
+  dplyr::mutate(cwd = case_when(site_cwd_zscore == -1 ~ "cool/wet site",
+                                site_cwd_zscore == 0 ~ "average site",
+                                site_cwd_zscore == 1 ~ "hot/dry site")) %>% 
+  dplyr::mutate(cwd = factor(cwd, levels = c("cool/wet site", "average site", "hot/dry site")))
+
+pipo_tpha_qmd_cwd_interaction_gg_raster <-
+  ggplot(fitted_subset, aes(x = pipo_and_dead_tpha_s, y = pipo_and_dead_qmd_s, fill = prob_pipo_mortality)) +
+  geom_raster() +
+  scale_fill_viridis_c() +
+  facet_grid(~ cwd) +
+  theme_bw() +
+  labs(x = "Ponderosa pine density\n(trees per hectare; scaled)",
+       y = "Ponderosa pine quadratic mean diameter\n(cm; scaled)",
+       fill = "Probability of mortality\nof ponderosa pine")
+
+pipo_tpha_qmd_cwd_interaction_df <-
+  fitted_compact %>% 
+  dplyr::filter(site_cwd_zscore %in% c(-1, 0, 1)) %>% 
+  dplyr::filter(overall_tpha_s == 0) %>% 
+  dplyr::filter(overall_qmd_s == 0) %>% 
+  dplyr::filter(abs(abs(pipo_and_dead_qmd_s) - 0.7) < 0.05) %>% 
+  dplyr::mutate(pipo_and_dead_qmd = ifelse(pipo_and_dead_qmd_s == -0.7, yes = "Smaller trees", no = "Larger trees")) %>% 
+  dplyr::mutate(cwd = case_when(site_cwd_zscore == -1 ~ "cool/wet site",
+                                site_cwd_zscore == 0 ~ "average site",
+                                site_cwd_zscore == 1 ~ "hot/dry site")) %>% 
+  dplyr::mutate(cwd = factor(cwd, levels = c("cool/wet site", "average site", "hot/dry site")))
+
+pipo_tpha_qmd_cwd_interaction_gg <-
+  ggplot(pipo_tpha_qmd_cwd_interaction_df, 
+         aes(x = pipo_and_dead_tpha_s, 
+             y = est_mn, 
+             color = pipo_and_dead_qmd)) +
+  geom_ribbon(aes(ymin = lwr, ymax = upr, group = as.factor(pipo_and_dead_qmd_s)), fill = "lightgray", color = NA) +
+  geom_line() +
+  facet_grid(~ cwd) +
+  theme_bw() +
+  labs(x = "Ponderosa pine density\n(trees per hectare; scaled)",
+       y = "Pr(ponderosa mortality)",
+       color = "Quadratic\nmean diameter")
+
+ggsave(plot = pipo_tpha_qmd_cwd_interaction_gg, filename = "figures/pipo_tpha_qmd_cwd_interaction.png")
+
+ggsave(plot = pipo_tpha_qmd_cwd_interaction_gg_raster, filename = "figures/pipo_tpha_qmd_cwd_interaction_raster.png")
+
+
+# halfeye plots of model coefficients -------------------------------------
+original_names <- colnames(samps)
+replacement_names <- c("Intercept", 
+                       "Site CWD", 
+                       "Ponderosa density", 
+                       "Ponderosa mean size", 
+                       "Overall density", 
+                       "Overall mean size", 
+                       "Site CWD : Ponderosa density",
+                       "Site CWD : Ponderosa mean size",
+                       "Ponderosa density : Ponderosa mean size",
+                       "Site CWD : Overall density",
+                       "Site CWD : Overall mean size",
+                       "Overall density : Overall mean size",
+                       "Site CWD : Ponderosa density : Ponderosa mean size",
+                       "Site CWD : Overall density : Overall mean size")
+
+replacement_name_order <- c(1, 2, 3, 5, 4, 6, 9, 12, 7, 10, 8, 11, 13, 14)
+colnames(samps) <- replacement_names
+long_samps <-
+  samps %>% 
+  tidyr::gather(key = "variable", value = "samps") %>% 
+  dplyr::mutate(variable = factor(variable,
+                                  levels = replacement_names[rev(replacement_name_order)]))
+
+effect_sizes_halfeye <-
+  ggplot(long_samps, aes(x = samps, y = variable)) +
+  geom_halfeyeh() +
+  theme_bw() +
+  geom_vline(xintercept = 0, lty = 2) +
+  labs(x = "Effect size\nLog odds change in Pr(Ponderosa mortality)\nfor a 1 standard deviation increase in covariate", y = "Coefficient")
+
+ggsave(plot = effect_sizes_halfeye, filename = "figures/effect-sizes-halfeye.png")
