3bc33cff140720ecb916d3b4c5763e370a27909c
Michael Koontz <mikoontz@gmail.com>
2019-04-06 15:54:44 -0600

    calculate accuracy for the witheld data for live/dead, species, and host/non-host classifiers
    Former-commit-id: 15d819e65eaecb890727fcb3d073fdd8239d9b95
    Former-commit-id: 7ce350910d46395a4da799ba038cee2a9fc83574
    Former-commit-id: 0592850c82951b523f4c0f7d283ea4ee56cbec66
    Former-commit-id: 314cccd84106cd0cb0dd60669b59bda3dde3b8ea

--- analyses/classification-summary-stats.R
+++ analyses/classification-summary-stats.R
@@ -4,6 +4,9 @@ library(tidyverse)
 library(caret)
 
 crowns_with_reflectance <- readr::read_csv(here::here("data/data_output/classified/hand-classified/hand-classified-crowns.csv"))
+crowns_with_reflectance <-
+  crowns_with_reflectance %>% 
+  dplyr::mutate(live = ifelse(live == 1, yes = "live", no = "dead"))
 
 # create the live/dead classifier -----------------------------------------
 live_or_dead_idx <- caret::createDataPartition(crowns_with_reflectance$live, p = 0.8, list = FALSE)
@@ -14,18 +17,28 @@ live_or_dead_testing <- crowns_with_reflectance[-live_or_dead_idx, ]
 live_or_dead_fit <- train(
   as.factor(live) ~ b_mean + g_mean + r_mean + re_mean + nir_mean + ndvi_mean + rgi_mean + cire_mean + cig_mean + ndre_mean,
   data = live_or_dead_training,
-  method = "LogitBoost"
+  method = "LogitBoost",
+  trControl = trainControl(classProbs = TRUE, 
+                            summaryFunction = twoClassSummary),
+  metric = "ROC"
 )
 
 # Classifier is called `live_or_dead_fit`
 live_or_dead_fit
 
+live_or_dead_predict <- predict(live_or_dead_fit, newdata = live_or_dead_testing)
+
+live_or_dead_pred_accuracy <- 
+  data.frame(data = live_or_dead_testing$live, pred = live_or_dead_predict) %>% 
+  dplyr::mutate(match = data == pred)
+
+mean(live_or_dead_pred_accuracy$match)
 
 # subset to only the live crowns ------------------------------------------
 
 live_crowns <- 
   crowns_with_reflectance %>% 
-  dplyr::filter(live == 1) %>% 
+  dplyr::filter(live == "live") %>% 
   dplyr::mutate(functional_group = case_when(species == "pila" ~ "pinus",
                                              species == "pipo" ~ "pinus",
                                              TRUE ~ species))
@@ -52,3 +65,21 @@ rdaFit <- train(
 
 # species classifier is called rdaFit
 rdaFit
+
+species_predict <- predict(rdaFit, newdata = species_testing)
+
+species_pred_accuracy <- 
+  data.frame(data = species_testing$species, pred = species_predict) %>% 
+  dplyr::mutate(match = data == pred) %>% 
+  dplyr::mutate(data_host = ifelse(data == "pipo", yes = "host", no = "non-host"),
+                pred_host = ifelse(pred == "pipo", yes = "host", no = "non-host")) %>% 
+  dplyr::mutate(match_host = data_host == pred_host)
+
+mean(species_pred_accuracy$match)
+mean(species_pred_accuracy$match_host)
+
+classification_summary_stats <-
+  tibble(type = c("live/dead", "species", "host/non-host"),
+         accuracy = c(mean(live_or_dead_pred_accuracy$match), mean(species_pred_accuracy$match), mean(species_pred_accuracy$match_host)))
+
+write_csv(classification_summary_stats, path = "analyses/analyses_output/classification-summary-stats.csv")
