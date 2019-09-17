c60e4f14df68976d6ea09e53b83e4f0dd17aa3b3
Michael Koontz <mikoontz@gmail.com>
2019-09-17 09:51:28 -0600

    simple script to summarize model and allow me to quickly grab the beta coefficient estimates and 95 percent credible intervals
    Former-commit-id: df77597d914c0e1d9da3507f478c21cde3b3a54c
    Former-commit-id: 444275c1456eba1ae58dc952e55104387ae2dcfa
    Former-commit-id: d92a07f22c64efb93921ed73d4bee665a6543bca
    Former-commit-id: 8fb79ae8e9b5eff201ed229400d647ccd053eeed

---
+++ analyses/summarize-final-model.R
@@ -0,0 +1,19 @@
+# Purpose: summarize the effect sizes of the final model
+
+library(brms)
+library(tidyverse)
+
+fm1 <- readRDS("analyses/analyses_output/fitted-model_zibinomial_site-cwdZscore_pipo-tpha-qmd_overall-tpha-qmd_exact-gp-per-site_200-samples.rds")
+
+fm1
+summary(fm1)
+
+out <- 
+  fm1 %>% 
+  fixef() %>% 
+  as_tibble() %>% 
+  mutate(beta = rownames(fixef(fm1))) %>% 
+  rename(lwr0_025 = Q2.5, 
+         upr0_975 = Q97.5)
+
+write_csv(x = out, path = "analyses/analyses_output/final-model-summary.csv")
