649c3bd0507bfd5e3cfc7638d3eda8d2d1f3c5f8
Michael Koontz <mikoontz@gmail.com>
2020-03-23 10:29:32 -0600

    surveyed area re-homed as an L0 product; workflow script appropriately numbered in the order of running
    Former-commit-id: 20251c5f2200132df28d8b983478708c28233d3c
    Former-commit-id: d5f44d9da15248f61a1885861c4b75f7bb66fda2
    Former-commit-id: c9b7dcf1e8989c6fe8acc733beb3b9374844ac7b
    Former-commit-id: 096224498232276a3cce2ac9e2557e174615096b

--- workflow/07_get-buffered-survey-area.R
+++ workflow/07_get-buffered-survey-area.R
@@ -5,13 +5,8 @@ library(sf)
 library(tidyverse)
 library(here)
 
-source(here::here("data/data_carpentry/make-processing-checklist.R"))
+source(here::here("workflow/01_make-processing-checklist.R"))
 sites_checklist
-
-sites_to_process <-
-  sites_checklist %>% 
-  dplyr::filter(crowns_check) %>% 
-  dplyr::pull(site)
 
 # With a given buffer size, how big will the total area surveyed be?
 survey_area <-
@@ -20,9 +15,9 @@ survey_area <-
     
     # One site has additional restrictions on its flight bounds in order to avoid any chance of visible private property in the processed imagery.
     if(current_site == "stan_3k_2") {
-      site_bounds <- sf::st_read(here::here("data/data_output/site_data/stan_3k_2/stan_3k_2_mission-footprint/stan_3k_2_site-bounds_privacy.geoJSON"))
+      site_bounds <- sf::st_read(here::here("data/data_drone/L0/mission-footprint/site-bounds/stan_3k_2_site-bounds_privacy.geoJSON"))
     } else {
-      site_bounds <- sf::st_read(here::here(paste0("data/data_output/site_data/", current_site, "/", current_site, "_mission-footprint/", current_site, "_site-bounds.geoJSON")))
+      site_bounds <- sf::st_read(here::here(paste0("data/data_drone/L0/mission-footprint/site-bounds/", current_site, "_site-bounds.geoJSON")))
     }
     
     geometry <-
@@ -30,14 +25,8 @@ survey_area <-
       st_transform(3310) %>% 
       st_geometry()
 
-    # buffered_bounds <-
-    #   site_bounds %>% 
-    #   st_transform(3310) %>% 
-    #   st_buffer(-35)
-    # 
     return(geometry)
-    # return(st_area(buffered_bounds))
-    
+
   })) %>% 
   dplyr::mutate(geometry = st_as_sfc(purrr::flatten(geometry))) %>% 
   dplyr::select(site, geometry) %>% 
@@ -50,4 +39,4 @@ units(survey_area$buffered_survey_area) <- "ha"
 
 survey_area
 
-sf::st_write(obj = survey_area, dsn = "data/data_output/surveyed-area-3310.gpkg")
+sf::st_write(obj = survey_area, dsn = "data/data_drone/L0/surveyed-area-3310.gpkg")
