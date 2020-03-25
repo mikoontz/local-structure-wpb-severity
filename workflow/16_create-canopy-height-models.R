8d72b6a276c11a0383e61aad8423f1c3f7998064
Michael Koontz <mikoontz@gmail.com>
2020-03-25 16:03:36 -0600

    use new function names from {lidR}v3.0.0
    Former-commit-id: 668f940a504c88e63717117337ba3563479388bd
    Former-commit-id: d345b8a0a080b2b264bbdc9f118b0e9134210bf0
    Former-commit-id: f0d94747919eae1c35a609c1e3b37618d5f8c314
    Former-commit-id: af05c5b43ca37427ddf2b2a581aafe0dca2ef562

--- workflow/16_create-canopy-height-models.R
+++ workflow/16_create-canopy-height-models.R
@@ -1,11 +1,16 @@
 # Load packages
 library(RCSF)
-library(lidR)
 library(sf)
 library(tidyverse)
 library(purrr)
 library(raster)
 library(rgl)
+
+if (try(packageVersion("lidR")) != "3.0.0") {
+  remotes::install_github("Jean-Romain/lidR@devel")
+}
+
+library(lidR)
 
 source("workflow/01_make-processing-checklist.R")
 
@@ -13,9 +18,6 @@ sites_checklist
 
 # These sites were processed with their X3 and RedEdge imagery combined so some of their
 # output products will be in a slightly different place in the project directory
-merged_sites <- c("eldo_3k_2",
-                  "eldo_3k_3",
-                  "eldo_4k_2")
 
 sites_to_overwrite <- "all"
 sites_checklist$overwrite <- FALSE
@@ -76,10 +78,10 @@ for (i in seq_along(sites_to_process)) {
   current_site <- sites_to_process[i]
   
   current_point_cloud <- lidR::readLAS(files = here::here(paste0("data/data_drone/L1/dense-point-cloud/", current_site, "_dense-point-cloud.las")))
-
+  
   # Wipe clean all of Pix4D classification
   current_point_cloud@data[, Classification := 0L]
-
+  
   # Use the cloth simulation filter by Zhang et al. (2016) [http://www.mdpi.com/2072-4292/8/6/501/htm]
   # implemented in the lidR package to classify points in the point cloud as ground or non-ground
   
@@ -87,13 +89,13 @@ for (i in seq_along(sites_to_process)) {
   # when point cloud density is ~30 pts per m^2
   current_idx <- csf_parameters$site == current_site
   
-  current_point_cloud <- lidR::lasground(las = current_point_cloud, 
-                                         algorithm = csf(sloop_smooth = csf_parameters$sloop_smooth[current_idx], 
-                                                         class_threshold = csf_parameters$class_threshold[current_idx], 
-                                                         cloth_resolution = csf_parameters$cloth_resolution[current_idx],  
-                                                         rigidness = csf_parameters$rigidness[current_idx], 
-                                                         iterations = csf_parameters$iterations[current_idx], 
-                                                         time_step = csf_parameters$time_step[current_idx]))
+  current_point_cloud <- lidR::classify_ground(las = current_point_cloud, 
+                                               algorithm = csf(sloop_smooth = csf_parameters$sloop_smooth[current_idx], 
+                                                               class_threshold = csf_parameters$class_threshold[current_idx], 
+                                                               cloth_resolution = csf_parameters$cloth_resolution[current_idx],  
+                                                               rigidness = csf_parameters$rigidness[current_idx], 
+                                                               iterations = csf_parameters$iterations[current_idx], 
+                                                               time_step = csf_parameters$time_step[current_idx]))
   
   
   # Plot the classification of the point cloud for inspection
@@ -142,7 +144,7 @@ for (i in seq_along(sites_to_process)) {
   # Note that all of these outputs generated using R get written to the same place, regardless of whether the
   # output is derived from merged X3+RedEdge imagery versus just being derived from RedEdge imagery
   raster::writeRaster(x = chm, filename = here::here(paste0("data/data_drone/L2/chm/", current_site, "_chm.tif")), overwrite = TRUE)
-
+  
   print(paste0("...", current_site, " complete..."))
 }  
 
